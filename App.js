16020f80b3a7d5651c1eaa8f0c63d7e67d6a58a0
Max16 <max16@naver.com>
2021-12-23 15:30:16 +0900

    #2.5 Tab Navigation

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
-import React, { useState } from "react";
-import { Asset, useAssets } from "expo-asset";
-import { Text, View, Image } from "react-native";
+import React from "react";
+import { useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 export default function App() {
   const [assets] = useAssets([require("./profile.jpeg")]);
@@ -12,5 +13,9 @@ export default function App() {
   if (!assets || !loaded) {
     return <AppLoading />;
   }
-  return <Text>Hello World</Text>;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
