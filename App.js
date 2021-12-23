a24c85cde1ca018170440df6965a4ee2c444b28b
Max16 <max16@naver.com>
2021-12-23 14:13:42 +0900

    #2.2 AppLoading part Two

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Asset, useAssets } from "expo-asset";
+import { Text, View, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./profile.jpeg")); // local 에서 불러옴
+    await Image.prefetch(
+      "https://images.unsplash.com/photo-1568876694728-451bbf694b83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"
+    ); // url 로 불러옴. 추천X
   };
   if (!ready) {
     return (
