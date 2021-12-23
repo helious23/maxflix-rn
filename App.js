6356bc0e2a198adace69514a93900725bddad8c5
Max16 <max16@naver.com>
2021-12-24 08:44:45 +0900

    #2.10 The Navigation Props

--- App.js
+++ App.js
@@ -1,50 +1,23 @@
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
-import React, { useState } from "react";
-import { Asset } from "expo-asset";
+import React from "react";
+import { useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
-import {
-  DarkTheme,
-  DefaultTheme,
-  NavigationContainer,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
-import { Image, useColorScheme } from "react-native";
-
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
+import Stack from "./navigation/Stack";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./profile.jpeg"),
-      "https://images.unsplash.com/photo-1568876694728-451bbf694b83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80",
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
-  const isDark = useColorScheme() === "dark";
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./profile.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
+
   return (
-    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
-      <Tabs />
+    <NavigationContainer>
+      <Stack />
     </NavigationContainer>
   );
 }
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -9,11 +10,70 @@ import colors from "../colors";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="TV" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? colors.NAVY : "white",
+        },
+        tabBarActiveTintColor: isDark ? colors.ORANGE : colors.NAVY,
+        tabBarInactiveTintColor: isDark ? colors.DARK_GREY : colors.LIGHT_GREY,
+        headerStyle: {
+          backgroundColor: isDark ? colors.NAVY : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : colors.NAVY,
+        },
+        tabBarLabelStyle: {
+          marginBottom: 3,
+          fontSize: 11,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name={"film-outline"}
+                color={color}
+                size={focused ? 29 : size}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={"tv-outline"}
+              color={color}
+              size={focused ? 29 : size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "search" : "search-outline"}
+              color={color}
+              size={focused ? 29 : size}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
