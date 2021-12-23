4e39048989003761cc7ddd2e021b1dd2f851af97
Max16 <max16@naver.com>
2021-12-23 14:25:22 +0900

    #2.3. Apploading without hooks

--- App.js
+++ App.js
@@ -5,15 +5,26 @@ import { Asset, useAssets } from "expo-asset";
 import { Text, View, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./profile.jpeg")); // local 에서 불러옴
-    await Image.prefetch(
-      "https://images.unsplash.com/photo-1568876694728-451bbf694b83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"
-    ); // url 로 불러옴. 추천X
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./profile.jpeg"),
+      "https://images.unsplash.com/photo-1568876694728-451bbf694b83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
