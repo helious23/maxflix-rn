3470d54fd59ace47b264e72767b1042736acee7a
Max16 <max16@naver.com>
2021-12-24 13:53:24 +0900

    #2.16 Typescript

---
+++ screens/Movies.tsx
@@ -0,0 +1,6 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import styled from "styled-components/native";
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => null;
+
+export default Movies;
